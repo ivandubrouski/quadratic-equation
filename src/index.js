1c9804687eaffd46bcbf0a1878656c353d36a7f1
Ivan <ivan.dubrouski@gmail.com>
2018-09-19 15:43:54 +0300

    quadratic-equation/done: my solution with regexp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  equation = equation.replace(/\s/g, '');
+  var temp = equation.match(/(-?\d+)\*x\^2(-?\+?\d+)\*x(-?\+?\d+)/);
+  var discriminant=temp[2]*temp[2]-4*temp[1]*temp[3];
+  var solution=[];
+  solution.push(+(-temp[2]/(2*temp[1])-Math.sqrt(discriminant)/(2*temp[1])).toFixed());
+  solution.push(+(-temp[2]/(2*temp[1])+Math.sqrt(discriminant)/(2*temp[1])).toFixed());
+  return (solution.sort((x1,x2) => x1-x2));
 }
